fb819ca4bccde09ee9f97322f89a3d28a608d526
Tobias Goldkette Prang <58465104+TGPGamez@users.noreply.github.com>
2022-01-24 19:45:26 +0100

    Canvas

--- Opgaver/GeometriOpgave/src/app/app.component.ts
+++ Opgaver/GeometriOpgave/src/app/app.component.ts
@@ -15,14 +15,36 @@ export class AppComponent {
   input2: number = 5;
 
   shape: Shape = new Rectangle(this.input1, this.input2);
+  
 
-  inputChange(event: any) : void {
-    console.log(this.input1);
+  inputChange(event: any) : void {    
     if ((this.input1 == 0 && this.input2 > 0) || (this.input2 == 0 && this.input1 > 0) || (this.input1 == this.input2)) {
       this.shape = new Square(this.input1 + this.input2);
+      this.drawSquare();
     } else {
       this.shape = new Rectangle(this.input1, this.input2);
+      this.drawRectangle();
     }
+  }
+
+  private drawRectangle() : void {
+    let canvas : any = document.getElementById("shapeCanvas");
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.rect(20, 20, 150, 100);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+  }
+
+  private drawSquare() : void {
+    let canvas : any = document.getElementById("shapeCanvas");
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.rect(50, 50, 80, 80);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
   }
 }
 
